667e0984ebc5fc575fc8513737cd9d44332c1f2c
rhit-yangnj <njy0809@gmail.com>
2024-08-19 12:31:37 -0500

    Update Plane.cs

--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -5,7 +5,7 @@ using UnityEngine.Events;
 
 public class Plane : MonoBehaviour
 {
-
+    public float camOffset = 1;
     private bool firstContact;
 
     private void Start()
@@ -18,7 +18,7 @@ public class Plane : MonoBehaviour
         if (firstContact)
         {
             Camera cam = Camera.main;
-            cam.GetComponent<CameraFollow>().setTarget(transform.position);
+            cam.GetComponent<CameraFollow>().setTarget(transform.position + new Vector3(0, camOffset, 0));
             GameManager gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
             gm.currentLayer.DisplaySelectionBar();
             firstContact = false;
