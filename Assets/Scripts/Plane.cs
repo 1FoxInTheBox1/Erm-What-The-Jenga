d44ddd9f75c350ea863d682fa21edc3799715883
rhit-yangnj <njy0809@gmail.com>
2024-08-19 12:42:31 -0500

    added comments

--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -13,13 +13,19 @@ public class Plane : MonoBehaviour
         firstContact = true;
     }
 
+    // In order to keep the camera centered on the new layer,
+    // The plane will tell the camera where it is the first time it hits something
+    // That something SHOULD be what the player just built
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Only run this the first time a collision occurs
         if (firstContact)
         {
             Camera cam = Camera.main;
+            // Set camera's target position to the plane's pos + offset
             cam.GetComponent<CameraFollow>().setTarget(transform.position + new Vector3(0, camOffset, 0));
             GameManager gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+            // Display the selection bar
             gm.currentLayer.DisplaySelectionBar();
             firstContact = false;
         }
