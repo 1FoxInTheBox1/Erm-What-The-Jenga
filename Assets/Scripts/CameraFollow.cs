b743f791893cfe8a75d8771ef196db6ef440bda9
Pyram1d <76640329+Pyram1d-dev@users.noreply.github.com>
2024-08-19 15:10:29 -0500

    Camera Lerp

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,36 +6,27 @@ public class CameraFollow : MonoBehaviour
 {
 
     Vector3 target;
-    Vector3 prevTarget;
     // Follow time is the time in seconds it will take for the camera to pan to its target position
     public float followTime = 1f;
-    float t;
 
     // Start is called before the first frame update
     void Start()
     {
         target = new Vector3(0, 0, -10);
-        prevTarget = new Vector3(0, 0, -10);
-        t = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // This is meant to be a simple linear interpolation
-        // Might be a better way of doing it but this seems to work just fine
-        Vector3 path = target - prevTarget;
-        float progress = Mathf.Clamp(t / followTime, 0, 1);
-        transform.position = prevTarget + (path * progress);
-        t += Time.deltaTime;
+        // Smooth camera :)
+        float delta = Mathf.Clamp(6 * Time.deltaTime, 0, 1);
+        transform.position = Vector3.Lerp(transform.position, target, delta);
     }
 
     // Sets the position the camera should pan over to
     public void setTarget(Vector3 newTarget)
     {
-        prevTarget = target;
         target = new Vector3(newTarget.x, newTarget.y, -10);
-        t = 0;
     }
 
     // This is just used to display the new layer's selection bar in the correct position
